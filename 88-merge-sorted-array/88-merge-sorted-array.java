e99d4d94699b109c221fc912e7e0e85f96e01f8a
Bharat Padhiyar <bpadhiyar4@gmail.com>
2022-08-12 23:27:26 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (49.87%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -5,10 +5,13 @@ class Solution {
         
         int k = nums1.length - 1;
         
+        //compare nums1, nums2 and keep placing the largest element in the kth position
         while(m >=0 && n>=0) {
             nums1[k--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
         }
         
+        //If there are elements in nums1, we don't have do anything since it is already sorted
+		//If there are elements in nums2, keep placing them in the front
         while(n >= 0) {
             nums1[k--] = nums2[n--];
         }
