3cdeab890e34af034f1fc32098baa92b0a537408
Bharat Padhiyar <bpadhiyar4@gmail.com>
2022-08-20 23:24:21 +0530

    Time: 1 ms (52.81%), Space: 41.7 MB (81.14%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -16,15 +16,43 @@
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();    
-        travel(root, ans);
-        return ans;
-    }
-    
-    private void travel(TreeNode root, List<Integer> preOrder) {
-        if(root != null) {
-            travel(root.left, preOrder);
-            travel(root.right, preOrder);
-            preOrder.add(root.val);
+        
+        TreeNode curNode = root;
+        
+        // untill current node is not null.
+        while(curNode != null) {
+            
+            // if current node's right is null, add the curNode val in list and shift curNode to curNode.left.
+            if(curNode.right == null) {
+                ans.add(curNode.val);
+                curNode = curNode.left;
+            } else {
+                
+                // create temporary between the left most node and curNode.
+                
+                // take preNode to the left most node.
+                TreeNode preNode = curNode.right;
+                while(preNode.left != null && preNode.left != curNode) {
+                    preNode = preNode.left;
+                }
+                
+                
+                // if the preNode.left is null then create a temporary link between preNode.left and shift curNode to curNode.right.
+                if(preNode.left == null) {
+                    preNode.left = curNode;
+                    ans.add(curNode.val);
+                    curNode = curNode.right;
+                } else {
+                    // preNode.left is curNode. Here we will set null to the preNode.left and the preNode.val in list and shift curNode to curNode.left.
+                    preNode.left = null;
+                    curNode = curNode.left;
+                }
+            }
         }
+        
+        // Reversing the list.
+        Collections.reverse(ans);
+        
+        return ans;
     }
 }
