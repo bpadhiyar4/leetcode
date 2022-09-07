479a7c63261e558b8a1a9c39ae318bb25d0623d6
Bharat Padhiyar <bpadhiyar4@gmail.com>
2022-09-08 00:21:34 +0530

    Time: 361 ms (23.88%), Space: 119 MB (11.81%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,30 +1,28 @@
 class Solution {
     
-    int resultStart;
-    int resultLength;
-    
     public String longestPalindrome(String s) {
-        int len = s.length();
-        for(int start =0; start<len;start++) {
-            expandRange(s, start, start);
-            expandRange(s, start, start+1);
+        int length = s.length();
+        int[][] dp = new int[length][length];
+        String ans = "";
+        int maxLength = 0;
+        for (int diff = 0; diff < length; diff++) {
+            for (int i = 0, j = i + diff; j < length; i++, j++) {
+                if (i == j) {
+                    dp[i][j] = 1;
+                } else if (diff == 1 && s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = 2;
+                } else if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1] != 0) {
+                    dp[i][j] = 2 + dp[i + 1][j - 1];
+                }
+
+                if (dp[i][j] != 0 && j - i + 1 > maxLength) {
+                    maxLength = j - i + 1;
+                    ans = s.substring(i, i+maxLength);
+                }
+            }
         }
-        
-        
-        return s.substring(resultStart, resultStart+resultLength);
-    }
-    
-    private void expandRange(String s, int begin, int end) {
-        int len = s.length();
-        while(begin >=0 && end <len && s.charAt(begin) == s.charAt(end)) {
-            begin--;
-            end++;
-        }
-        
-        if(resultLength < end - begin - 1) {
-            resultStart = begin+1;
-            resultLength = end - begin - 1;
-        }
+
+        return ans;
     }
     
 }
