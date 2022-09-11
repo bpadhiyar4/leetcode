026ac7b8927530627f9e7a2c4ffdfdce25faaa35
Bharat Padhiyar <bpadhiyar4@gmail.com>
2022-09-11 20:12:46 +0530

    Time: 24 ms (69.20%), Space: 42.7 MB (95.36%) - LeetHub

--- 18-4sum/18-4sum.java
+++ 18-4sum/18-4sum.java
@@ -1,46 +1,45 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-    List<List<Integer>> ans = new ArrayList<>();
-    if(nums.length<4)
-        return ans;
-    Arrays.sort(nums);
-    if(nums[0]>0 && target<0)
-        return ans;
-    for(int i=0;i<nums.length;i++){
+        List<List<Integer>> ans = new ArrayList<>();
         
-        while(i!=0 && i<nums.length && nums[i]==nums[i-1])
-            ++i;
-        for(int j=i+1;j<nums.length;j++){
-            while(j<nums.length && j-1!=i && nums[j]==nums[j-1])
-                ++j;
+        Arrays.sort(nums);
+        
+        for(int i=0;i<nums.length - 3; i++) {
+            while(i>0 && i < nums.length - 3 && nums[i] == nums[i-1]) {
+                i++;
+            }
             
-            int low = j+1;
-            int high=nums.length-1;
-            while(low<high){
+            for(int j = i+1; j<nums.length -2;j++) {
+                while(j > i+1 && j<nums.length-2 && nums[j] == nums[j-1]) {
+                    j++;
+                }
+
+                int k = j+1;
+                int l = nums.length -1;
                 
-                if(nums[i]+nums[j]+nums[low]+nums[high]==target){
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[i]);
-                    list.add(nums[j]);
-                    list.add(nums[low]);
-                    list.add(nums[high]);
-                    ans.add(list);
-                    while(low<high && nums[low]==nums[low+1])
-                        ++low;
-                    while(low<high && nums[high]==nums[high-1])
-                        --high;
-                    
-                    ++low;
-                    --high;
-                }else if (nums[i]+nums[j]+nums[low]+nums[high]<target)
-                    ++low;
-                else
-                    --high;
+                while(k<l) {
+                    if( (long)nums[i] + nums[j] + nums[k] + nums[l] == target) {
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        
+                        while(k < l && nums[k] == nums[k+1]) {
+                            k++;
+                        } 
+                        
+                        while(k < l && nums[l] == nums[l-1]) {
+                            l--;
+                        }
+                        
+                        k++;
+                        l--;
+                    } else if((long)nums[i] + nums[j] + nums[k] + nums[l] > target){
+                        l--;
+                    } else {
+                        k++;
+                    }
+                }
             }
         }
-    }
         
-    
-    return ans;
+        return ans;
     }
 }
