6656132985aafbcff25c67875fec4b68309a878e
Bharat Padhiyar <bpadhiyar4@gmail.com>
2022-09-15 23:34:02 +0530

    Time: 8 ms (27.48%), Space: 44.7 MB (22.40%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -5,14 +5,20 @@ class Solution {
         
         Deque<Integer> decStack = new LinkedList<>();
         
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        
+        for(int index=0;index<nums1.length;index++) {
+            indexMap.put(nums1[index], index);
+        }
+        
         for(int index = nums2.length -1; index >= -1; index--) {
             
             while(!decStack.isEmpty() && (index == -1 || nums2[decStack.peek()] < nums2[index])) {
                 int popIndex = decStack.pop();
                 if(!decStack.isEmpty()) {
                     int prevIndex = decStack.peek();
-                    int indexForAns = getIndex(nums1, nums2[popIndex]);
-                    
+                    int indexForAns = indexMap.getOrDefault(nums2[popIndex], -1);
+
                     if(indexForAns != -1) {
                         ans[indexForAns] = nums2[prevIndex];
                     }
