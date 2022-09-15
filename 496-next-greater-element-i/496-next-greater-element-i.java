f5f1efadd3345f077c5bfcc336cc69a5b84f7114
Bharat Padhiyar <bpadhiyar4@gmail.com>
2022-09-15 23:27:04 +0530

    Time: 11 ms (11.26%), Space: 44.2 MB (59.34%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] ans = new int[nums1.length];
+        Arrays.fill(ans, -1);
+        
+        Deque<Integer> decStack = new LinkedList<>();
+        
+        for(int index = nums2.length -1; index >= -1; index--) {
+            
+            while(!decStack.isEmpty() && (index == -1 || nums2[decStack.peek()] < nums2[index])) {
+                int popIndex = decStack.pop();
+                if(!decStack.isEmpty()) {
+                    int prevIndex = decStack.peek();
+                    int indexForAns = getIndex(nums1, nums2[popIndex]);
+                    
+                    if(indexForAns != -1) {
+                        ans[indexForAns] = nums2[prevIndex];
+                    }
+                }
+            } 
+            
+            decStack.push(index);
+        }
+        
+        return ans;
+    }
+    
+    private int getIndex(int[] nums, int ele) {
+        
+        for(int i=0;i<nums.length; i++) {
+            if(nums[i] == ele) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
