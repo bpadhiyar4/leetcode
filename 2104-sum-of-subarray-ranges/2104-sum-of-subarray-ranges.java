9ddfcdcc39990e4aff0c2e7b2635a5563f8e3c34
Bharat Padhiyar <bpadhiyar4@gmail.com>
2022-09-15 21:33:10 +0530

    Time: 33 ms (38.17%), Space: 50.1 MB (6.48%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,18 +1,28 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
+        Stack<Integer> decStack = new Stack<>();
+        Stack<Integer> incStack = new Stack<>();
+        
         long sum = 0;
         
-        for(int i=0; i< nums.length-1;i++) {
-            int max = nums[i];
-            int min = nums[i];
-            
-            for(int j=i+1;j<nums.length;j++) {
-                max = Math.max(max, nums[j]);
-                min = Math.min(min, nums[j]);
-                
-                sum += max - min;
-            }
+        for(int i=0;i<=nums.length;i++) {
+            sum += pushToStack(decStack, nums, i, (a,b) -> a < b) - pushToStack(incStack, nums, i, (a,b) -> a > b);
         }
+        
+        return sum;
+    }
+    
+    private long pushToStack(Stack<Integer> stack, int[] nums, int index, BiFunction<Integer, Integer, Boolean> compare) {
+        long sum = 0;
+        
+        while(!stack.isEmpty() && (index == nums.length || compare.apply(nums[stack.peek()], nums[index]))) {
+            int popIndex = stack.pop();
+            int prevIndex = stack.isEmpty() ? -1 : stack.peek();
+            
+            sum += (popIndex - prevIndex) * (index - popIndex) * (long)nums[popIndex];
+        }
+        
+        stack.push(index);
         
         return sum;
     }
