601559d170e5646501326288d9b056e17bb7a6cd
Bharat Padhiyar <bpadhiyar4@gmail.com>
2024-09-08 23:50:52 +0530

    Time: 2 ms (52.18%), Space: 41.4 MB (54.15%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> paraStack = new Stack<>();
+        int len = s.length();
+        for (int i=0; i < len; i++) {
+            Character currCh = s.charAt(i);
+            if (currCh == '{' || currCh == '(' || currCh == '[') {
+                paraStack.push(currCh);
+                continue;
+            } else if (paraStack.size() == 0) {
+                return false;
+            }
+
+            Character popChar = paraStack.pop();
+            if ((currCh == '}' && popChar != '{') || 
+                (currCh == ')' && popChar != '(') || 
+                (currCh == ']' && popChar != '[')
+                ) {
+                    return false;
+                }
+        }
+
+        return paraStack.size() == 0 ? true : false;
+    }
+}
