7e36075031ec98a7d653b1ef966efe0b095827ee
Bharat Padhiyar <bpadhiyar4@gmail.com>
2024-09-13 00:38:45 +0530

    Time: 0 ms (100%), Space: 41.4 MB (69.29%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] bucket = new int[n+1];
+
+        for(int citation : citations) {
+            if (citation >= n) {
+                bucket[n]++;
+            } else {
+                bucket[citation]++;
+            }
+        }
+
+        int hIndex = 0;
+
+        for (int i = n; i>= 0; i--) {
+            hIndex += bucket[i];
+            if (hIndex >= i) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+}
