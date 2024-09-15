133dbc687343f343d146b37b398c4fe43219a5f8
Bharat Padhiyar <bpadhiyar4@gmail.com>
2024-09-16 00:23:40 +0530

    Time: 2 ms (98.41%), Space: 56.4 MB (71.59%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -3,24 +3,21 @@ class Solution {
         int len = gas.length;
         int gasSum = 0;
         int costSum = 0;
-
+        int tank = 0;
+        int ans = 0;
         for (int i=0; i<len;i++) {
             gasSum += gas[i];
             costSum += cost[i];
-        }
 
-        if (gasSum < costSum) {
-            return -1;
-        }
-
-        int tank = 0;
-        int ans = 0;
-        for (int i=0;i<len;i++) {
             tank += gas[i] - cost[i];
             if (tank < 0) {
                 ans = i + 1;
                 tank = 0;
             }
+        }
+
+        if (gasSum < costSum) {
+            return -1;
         }
 
         return ans;
