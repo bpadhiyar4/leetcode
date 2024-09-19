f7e617e06db4faf19d9fc8c8ab3310bd9c05b155
Bharat Padhiyar <bpadhiyar4@gmail.com>
2024-09-19 23:58:05 +0530

    Time: 15 ms (24.77%), Space: 45.1 MB (42.21%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int ransomLen = ransomNote.length();
+        int magLen = magazine.length();
+
+        Map<Character, Integer> ransomMap = new HashMap();
+        Map<Character, Integer> magMap = new HashMap();
+
+        for (int i = 0; i < ransomLen; i++) {
+            char currChar = ransomNote.charAt(i);
+            ransomMap.put(currChar, ransomMap.getOrDefault(currChar, 0) + 1);
+        }
+
+        for (int i = 0; i < magLen; i++) {
+            char currChar = magazine.charAt(i);
+            magMap.put(currChar, magMap.getOrDefault(currChar, 0) + 1);
+        }
+
+        for (Map.Entry<Character, Integer> ran : ransomMap.entrySet()) {
+            int count = magMap.getOrDefault(ran.getKey(), 0);
+            if (count < ran.getValue()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
