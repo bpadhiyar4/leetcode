c7a00a7dd15da00d0cfdfeee099c1a12d1720aa9
Bharat Padhiyar <bpadhiyar4@gmail.com>
2024-09-22 19:23:54 +0530

    Time: 7 ms (83.43%), Space: 42.4 MB (55.43%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -3,7 +3,6 @@ class Solution {
         int sLen = s.length();
 
         Map<Character, Character> map = new HashMap<>();
-        Set<Character> set = new HashSet<>();
         for(int i=0; i < sLen; i++) {
             Character tCh = t.charAt(i);
             Character sCh = s.charAt(i);
@@ -14,11 +13,8 @@ class Solution {
                 }
 
                 map.put(sCh, tCh);
-                // set.add(tCh);
-            } else {
-                if (mapped != tCh) {
-                    return false;
-                }
+            } else if (mapped != tCh) {
+                return false;
             }
         }
 
