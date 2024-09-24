fd5b1ac2539000e0526b79bc208376e98a1ab4ab
Bharat Padhiyar <bpadhiyar4@gmail.com>
2024-09-25 00:49:01 +0530

    Time: 0 ms (100%), Space: 42.6 MB (34.73%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -25,16 +25,12 @@ class Solution {
             }
         }
 
-        while(list1 != null) {
+        if(list1 != null) {
             tail.next = list1;
-            tail = tail.next;
-            list1 = list1.next;
         }
 
-        while(list2 != null) {
+        if(list2 != null) {
             tail.next = list2;
-            tail = tail.next;
-            list2 = list2.next;
         } 
 
         return head.next;
