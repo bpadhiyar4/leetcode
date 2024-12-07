0f928e0ad5de7e46c43a705536ec55d81d43c7b7
Bharat Padhiyar <bpadhiyar4@gmail.com>
2024-12-07 13:24:04 +0530

    Time: 0 ms (100%), Space: 39.8 MB (96.8%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -3,13 +3,15 @@ class Solution {
         if (n == 1) {
             return 1;
         }
-        int[] cache = new int[n];
-        cache[n-1] = 1;
-        cache[n-2] = 1;
+
+        int last = 1;
+        int secondLast = 1;
         for(int i=n-3; i>=0; i--) {
-            cache[i] = cache[i+1] + cache[i+2];
+            int temp = last + secondLast;
+            last = secondLast;
+            secondLast = temp;
         }
 
-        return cache[0] + cache[1];
+        return last + secondLast;
     }
 }
