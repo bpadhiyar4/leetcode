fb6ebf91c6a8a512de939d848497a4ffbb93db4f
Bharat Padhiyar <bpadhiyar4@gmail.com>
2025-01-20 21:55:28 +0530

    Time: 0 ms (100%), Space: 41.2 MB (6.13%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int fib(int n) {
+        
+        if (n < 2) {
+            return n;
+        }
+
+        int first = 0;
+        int second = 1;
+
+        for (int i=2;i<=n;i++) {
+            int temp = first + second;
+            first = second;
+            second = temp;
+        }
+
+        return second;
+    }
+}
